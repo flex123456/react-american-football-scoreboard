0487d34dcd81e5f388f4b9b6e5cbce48818b8f5f
milano12345 <dmilano857@gmail.com>
2019-08-12 15:45:37 -0500

    created timer functionality={working}

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
-import Timer from './Components/timer'
+
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 const [homeScore, setHomeScore] = useState(0)
 const [awayScore, setAwayScore] = useState(0)
+const [seconds, setSeconds] = useState(0);
+const [isActive, setIsActive] = useState(false);
+function toggle() {
+  setIsActive(!isActive);
+}
+
+function reset() {
+  setSeconds(0);
+  setIsActive(false);
+}
+useEffect(() => {
+  let interval = null;
+  if (isActive) {
+    interval = setInterval(() => {
+      setSeconds(seconds => seconds + 1);
+    }, 1000);
+  } else if (!isActive && seconds !== 0) {
+    clearInterval(interval);
+  }
+  return () => clearInterval(interval);
+}, [isActive, seconds]);
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -19,7 +41,7 @@ const [awayScore, setAwayScore] = useState(0)
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{seconds}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
@@ -38,8 +60,8 @@ const [awayScore, setAwayScore] = useState(0)
           <button className="awayButtons__fieldGoal" onClick={() => setAwayScore(awayScore + 3)}>Away Field Goal</button>
         </div>
         <div className="timerButtons">
-          <button className="timerStart" onClick={()=> setAwayScore(awayScore + 7)}>Start Timer</button>
-          <button className="clearTimer" onClick={() => setAwayScore(awayScore + 3)}>Clear Timer</button>
+          <button className="timerStart" onClick={toggle}>Start Timer</button>
+          <button className="clearTimer" onClick={reset}>Clear Timer</button>
         </div>
       </section>
     </div>
